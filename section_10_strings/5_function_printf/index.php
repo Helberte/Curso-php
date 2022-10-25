34a80630965688e48387f0ca1e8152b06a8d9351
helberte costa arruda <helberteads@gmail.com>
2022-10-24 21:35:15 -0400

    inicio sessão 10 - avançando em strings

---
+++ section_10_strings/5_function_printf/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+// para strings é %s
+// para inteiros é %d
+// para float é %f
+
+$nome = "Matheus";
+$teste = 15;
+
+printf("O nome %d de fulano é ", $teste);
+echo("<br>");
+
+// numeros quebrados
+printf("Olá pessoal %.2f ", 12.58);
+
+?>
---
+++ section_9/18_var_dump/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$arr = [
+  "Helberte",
+  "costa",
+  "arruda",
+  24,
+  "casado"];
+
+
+$x = 10;
+var_dump($x);
+
+
+?>
