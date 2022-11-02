ce8d621f762d2bf66b4b5a791efc5fdf3de7ac6c
helberte costa arruda <helberteads@gmail.com>
2022-11-02 18:36:27 -0400

    traits

---
+++ section_12_orientação_Objetos_OOP/16_traits/index.php
@@ -0,0 +1,37 @@
+<?php
+
+// a palavra reservada traits significa que podemos chamar especificamentte
+// aquela classe dentro de outra usando a palavra use
+// desta forma fica muito claro que estamos utilizando
+// os metodos de uma classe especifica
+
+trait Objeto{
+
+  public $y = 10;
+
+  public function teste(){
+    echo "testando traits de objeto <br>";
+  }
+}
+
+trait Testando{
+  public function traitTeste(){
+    return "este metodo está retornando string";
+  }
+}
+
+class Central{
+  use Objeto;
+  use Testando;
+}
+
+$x = new Central;
+
+$x->teste();
+echo $x->traitTeste();
+echo "<br>";
+echo $x->y;
+
+
+
+?>
