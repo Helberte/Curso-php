cde58f31333fe4f3af17ae124c1f9e4cf46ce45e
helberte costa arruda <helberteads@gmail.com>
2022-11-02 20:11:57 -0400

    Verificando objetos

---
+++ section_12_orientação_Objetos_OOP/22_verificando_objetos/index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Pessoa{
+
+  public function falar(){}
+}
+
+$eu = new Pessoa;
+
+var_dump(is_object($eu)); // verifica se a variável é um objeto
+echo "<br>";
+
+echo get_class($eu); // retorna a classe do objeto;
+echo "<br>";
+
+var_dump(method_exists($eu, "falar")); // retorna se o metodo existe ou não
+
+?>
